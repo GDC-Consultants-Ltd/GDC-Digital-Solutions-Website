400bdbd5f62a271d8bf17560d923c511b7b961e2
Malshi-K <malshi.k@gdcgroup.co.nz>
2025-03-17 14:22:25 +0530

    build error fixed

--- app/success/page.js
+++ app/success/page.js
@@ -48,7 +48,7 @@ export default function SuccessPage() {
             </div>
             <h1 className="text-2xl font-bold text-gray-800 mb-4">Thank You!</h1>
             <p className="text-gray-600 mb-6">
-              Your message has been received successfully. We'll get back to you as soon as possible.
+              Your message has been received successfully. We&apos;ll get back to you as soon as possible.
             </p>
             <Link 
               href="/" 
